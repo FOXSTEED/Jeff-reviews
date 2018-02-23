ef227f57fb795b4cb5ffcd31f816a83cb8e59aa1
Bryan Duong <bryandduong@gmail.com>
2018-02-23 14:08:53 -0800

    created fake data into db

--- database/index.js
+++ database/index.js
@@ -1,7 +1,42 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/reviews');
+const dataGen = require('./seedData.js');
 
 const db = mongoose.connection;
 db.once('open', function() {
   console.log('DB is running!');
 });
+
+let reviewSchema = mongoose.Schema({
+  listingId: Number,
+  reviewId: {type: Number, unique: true},
+  date: String,
+  rating: Number,
+  reviewHeadline: String,
+  comment: String,
+  userName: String,
+  userLocation: String,
+  userImage: String
+});
+
+let Review = mongoose.model('Review', reviewSchema);
+
+let save = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let entry = arr[i];
+    let toSave = new Review({
+      listingId: entry.listingId,
+      reviewId: entry.reviewId,
+      date: entry.date,
+      rating: entry.rating,
+      reviewHeadline: entry.reviewHeadline,
+      comment: entry.comment,
+      userName: entry.userName,
+      userLocation: entry.userLocation,
+      userImage: entry.userImage
+    });
+    toSave.save();
+  }
+}
+
+module.exports.save = save;
