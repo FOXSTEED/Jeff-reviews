683004bda99461d85d08b02071af473308a73a0e
Bryan Duong <bryandduong@gmail.com>
2018-02-26 17:10:08 -0800

    added another db test

--- testing/database.test.js
+++ testing/database.test.js
@@ -9,4 +9,15 @@ describe('Checking database', () => {
       expect(reviews.length).toBe(1000);
     });
   });
+
+  test('Every listing has atleast 1 review', () => {
+    query.review.find((err, reviews) => {
+      if (err) {
+        console.error(err);
+      }
+      let listings = [];
+      reviews.forEach(review => listings.push(review.listingId));
+      expect(listings.length).toBe(200);
+    });
+  });
 });
