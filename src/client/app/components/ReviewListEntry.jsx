829d115d6af5040cbea674a90698ef7767c308b4
Bryan Duong <bryandduong@gmail.com>
2018-02-28 12:34:38 -0800

    basic styling for reviews are in

--- src/client/app/components/ReviewListEntry.jsx
+++ src/client/app/components/ReviewListEntry.jsx
@@ -3,23 +3,25 @@ import styles from './styling/ReviewListEntry.css';
 
 const ReviewListEntry = (props) => {
   return (
-    <div>
+    <div className={styles.main}>
       <div className={styles.user}>
         <img src={props.review.userImage} className={styles.picture}/>
         <p className={styles.username}>{props.review.userName}</p>
         <p className={styles.location}>{props.review.userLocation}</p>
       </div>
       <div>
-        <span>{`${props.review.rating} STAR(S)!`}</span>
-        <span>{`Reviewed on ${props.review.date}`}</span>
-      </div>
-      <div className={styles.comment}>
-        <p className={styles.headline}>{props.review.reviewHeadline}</p>
-        <p className={styles.review}>{props.review.comment}</p>
-      </div>
-      <div>
-        <a href='#'>{`Ask ${props.review.userName} about Pearl Harbor`}</a>
-        <p><em>This review is the subjective opinion of a TripAdvisor member and not of TripAdvisor LLC.</em></p>
+        <div>
+          <span>{`${props.review.rating} STAR(S)!`}</span>
+          <span>{`Reviewed on ${props.review.date.slice(0,15)}`}</span>
+        </div>
+        <div className={styles.comment}>
+          <p className={styles.headline}>{props.review.reviewHeadline}</p>
+          <p className={styles.review}>{props.review.comment}</p>
+        </div>
+        <div>
+          <a href='#' className={styles.ask}>{`Ask ${props.review.userName} about Pearl Harbor`}</a>
+          <p className={styles.bottomnote}><em>This review is the subjective opinion of a Nomad member and not of Nomad LLC.</em></p>
+        </div>
       </div>
     </div>
   )
